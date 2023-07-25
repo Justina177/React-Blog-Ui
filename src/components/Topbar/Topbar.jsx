caebf04675ff608446700e28d7e01dd44c08411e
justina177 <justinaenenche@yahoo.com>
2023-07-26 00:50:32 +0100

    initial commit

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Topbar.css';
+
+export default function Topbar() {
+  return (
+    <div className="top">
+      <div className="topLeft"></div>
+      <div className="topCenter"></div>
+      <div className="topRight"></div>
+    </div>
+  )
+}
