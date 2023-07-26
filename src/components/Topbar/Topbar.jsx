15b06b298978ed4aea010a5a0129550a1bbe519c
justina177 <justinaenenche@yahoo.com>
2023-07-26 02:07:59 +0100

    Added topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Topbar.css';
+import img1 from '../../Img/f4.jpg'
 
 export default function Topbar() {
   return (
     <div className="top">
-      <div className="topLeft"></div>
-      <div className="topCenter"></div>
-      <div className="topRight"></div>
+      <div className="topLeft">
+          <i className="topIcon fa-brands fa-square-facebook"></i>
+          <i className="topIcon fa-brands fa-square-twitter"></i>
+          <i className="topIcon fa-brands fa-square-instagram"></i>
+          <i className="topIcon fa-brands fa-square-pinterest"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">Logout</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img src={img1} alt="" className="topimg" />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   )
 }
