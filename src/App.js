b03ad71af6baf44baa58dca381d615521af95e1b
justina177 <justinaenenche@yahoo.com>
2023-07-31 00:25:30 +0100

    Implemented routing in the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import Topbar from './components/Topbar/Topbar';
-// import Login from './pages/Login/Login';
+import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-// import Settings from './pages/Settings/Settings';
-// import Write from './pages/Write/Write';
-// import Single from './pages/Single/Single';
-// import Home from './pages/Home/Home';
+import Settings from './pages/Settings/Settings';
+import Write from './pages/Write/Write';
+import Single from './pages/Single/Single';
+import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Topbar /> 
-      {/* <Home />      */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </div>
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route path ="/" exact element={<Home />} />
+        <Route path ="/register" exact element={<Register />} />
+        <Route path ="/login" exact element={<Login />} />
+        <Route path ="/settings" exact element={<Settings />} />
+        <Route path ="/write" exact element={<Write />} />
+        <Route path ="/single" exact element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
